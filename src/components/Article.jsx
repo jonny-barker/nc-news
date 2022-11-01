cb76503c4824f5f9a7c5e179815b24a2945a4558
jonny-barker <jonny.d.barker@gmail.com>
2022-11-01 15:04:51 +0000

    added individual article page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,17 @@
 import { convertDate } from "../utils";
+import { Link } from "react-router-dom";
 
-export default function Article({ article }) {
+export default function Article ({ article }) {
   return (
-    <div
-      id="article-box"
-      className={article.article_id}>
-      <h2>{article.title}</h2>
-      <h3>{article.author}</h3>
-      <h4>{convertDate(article.created_at)}</h4>
-    </div>
+    <Link to={`/articles/${article.article_id}`}>
+      <div
+        id="article-box"
+        className={article.article_id}>
+        <h2>{article.title}</h2>
+        <h3>{article.author}</h3>
+        <h4>{convertDate(article.created_at)}</h4>
+      </div>
+    </Link>
   );
 }
+<div>Article Page</div>;
