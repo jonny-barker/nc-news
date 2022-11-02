e5998d537a62ef27d04cd91e8af1485a35d6fb7f
jonny-barker <jonny.d.barker@gmail.com>
2022-11-02 10:51:06 +0000

    added comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,10 @@
+import { convertDate } from "../utils"
 export default function Comment ({ comment }) {
-  console.log(comment);
   return (
     <div>
-      {comment.body}
+      <h4>{comment.author}</h4>
+      <h4>Written {convertDate(comment.created_at)}</h4>
+      <p>{comment.body}</p>
     </div>
   )
 }
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -4,19 +4,30 @@ import Comment from "./Comment";
 
 export default function Comments({ article }) {
   const [comments, setComments] = useState([]);
+  const [revealComments, setRevealComments] = useState(false);
 
   useEffect(() => {
     getComments(article.article_id).then((comments) => {
       setComments(comments);
     });
   }, [article.article_id]);
-
+  console.log(comments, "comments");
+  if (!revealComments) {
+    return (
+      <div>
+        <button onClick={() => setRevealComments(true)}>
+          Reveal Comments...
+        </button>
+      </div>
+    );
+  }
   return (
     <div>
+      <button onClick={() =>setRevealComments(false)}>Hide Comments...</button>
       <h3>Comments</h3>
       <ul>
         {comments.map((comment) => {
-          return <Comment comment={comment}/>
+          return <Comment comment={comment} />;
         })}
       </ul>
     </div>
